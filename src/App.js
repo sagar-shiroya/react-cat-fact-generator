c1c6bee052b1b52d40d8631ea236d9b2f1849ee8
Sagar Shiroya <sagar.shiroya8@gmail.com>
2023-03-06 17:44:19 +0530

    Changes for cat fact generator with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [fact, setFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{fact}</p>
     </div>
   );
 }
